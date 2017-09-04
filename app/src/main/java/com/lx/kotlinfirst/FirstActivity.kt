3f7615d242117306375ae4ded423f58eb07736b5
LiuXi0314 <xiaoxixizhizhi@gmail.com>
2017-09-04 12:49:45 +0800

    create repository

---
+++ app/src/main/java/com/lx/kotlinfirst/FirstActivity.kt
@@ -0,0 +1,14 @@
+package com.lx.kotlinfirst
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class FirstActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first)
+    }
+
+
+}
