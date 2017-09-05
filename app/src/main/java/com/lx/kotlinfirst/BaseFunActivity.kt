58ac26b37e072e20802cbbf0968c5cbdc77cce3f
LiuXi0314 <xiaoxixizhizhi@gmail.com>
2017-09-05 18:38:46 +0800

    Happy day

--- app/src/main/java/com/lx/kotlinfirst/BaseFunActivity.kt
+++ app/src/main/java/com/lx/kotlinfirst/BaseFunActivity.kt
@@ -1,12 +1,49 @@
 package com.lx.kotlinfirst
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.TextView
+import android.widget.Toast
 
 class BaseFunActivity : AppCompatActivity() {
+
+    private var textView: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_base_fun)
+        initView()
+
     }
+
+    private fun initView() {
+        findViewById(R.id.firstButton).setOnClickListener { testTextView() }
+        findViewById(R.id.secondButton).setOnClickListener { doSomeThing() }
+    }
+
+    private fun testTextView() {
+        textView = findViewById(R.id.firstButton) as TextView
+        textView!!.text = "第一次尝试构建一个button"
+        textView!!.setTextColor(getColor(R.color.colorAccent))
+        textView!!.textSize = 14f
+        textView!!.setOnClickListener { Toast.makeText(this, "无敌变换", Toast.LENGTH_LONG).show() }
+    }
+
+
+    private fun doSomeThing() {
+        val demo = object {
+            var x: Int = 10
+            var y = 11
+        }
+        var a = sum(1, 2)
+        var s = "一段文本"
+        Log.d("first", "$s.length = ${s.length}")
+        Log.d("first", "demo.x=${demo.x}")
+        Log.d("first", "demo.y=${demo.y}")
+
+        Log.d("first", "Hello.x=${Hello.x}")
+    }
+
+    private var sum: (a: Int, b: Int) -> Int = { x, y -> x + y }
 }
--- app/src/main/java/com/lx/kotlinfirst/FirstActivity.kt
+++ app/src/main/java/com/lx/kotlinfirst/FirstActivity.kt
@@ -1,8 +1,8 @@
 package com.lx.kotlinfirst
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 
 class FirstActivity : AppCompatActivity() {
 
@@ -11,26 +11,9 @@ class FirstActivity : AppCompatActivity() {
         setContentView(R.layout.activity_first)
 
         findViewById(R.id.baseFun).setOnClickListener {
-
+            startActivity(Intent().setClass(this, BaseFunActivity::class.java))
         }
 
-
-    }
-
-    private var sumaaaa: (a: Int, b: Int) -> Int = { x, y -> x + y }
-
-    fun doSomeThing(){
-        val demo = object {
-            var x : Int = 10
-            var y = 11
-        }
-        var a = sumaaaa(1, 2)
-        var s = "ssssssssssss"
-        Log.d("first", "$s.length = ${s.length}")
-        Log.d("first", "demo.x=${demo.x}")
-        Log.d("first", "demo.y=${demo.y}")
-
-        Log.d("first","Hello.x=${Hello.x}")
     }
 
 
